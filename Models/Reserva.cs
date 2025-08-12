4d9ebb3aebf5f4449e583c087e8ebf4a87fe384c
WintonOPF <wintonfilho@ufms.br>
2025-08-11 22:15:21 -0400

    Add project files.

---
+++ Models/Reserva.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospedagem.Entities
+{
+    public class Reserva
+    {
+        public Reserva() { }
+        public Reserva(int diasReservados)
+        {
+            this.DiasReservados = diasReservados;
+        }
+        public int DiasReservados { get; set; }
+        public List<Pessoa> Hospedes { get; set; }
+        public Suite Suite { get; set; }
+        public void CadastrarHospedes(List<Pessoa> hospedes)
+        {
+            Suite suite = new Suite();
+
+            if (hospedes.Count() > suite.Capacidade)
+            {
+                Hospedes = hospedes;
+            } 
+            else {
+                throw new Exception("A quantidade de hóspedes excede a capacidade da suíte.");
+            }
+        }
+        public void CadastrarSuite(Suite suite)
+        {
+            Suite = suite;
+        }
+
+        public int ObterQuantidadeHospedes()
+        {
+            return Hospedes.Count();
+        }
+
+        public decimal CalcularValorDiaria()
+        {
+            decimal valor = DiasReservados * Suite.ValorDiaria;
+            if (DiasReservados >= 10)
+            {
+                decimal desconto = (valor / 100) * 10;
+                valor -= desconto;
+            }
+            return valor;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using Hospedagem.Entities;
+
+List<Pessoa> hospedes = new List<Pessoa>();
+
+hospedes.Add(new Pessoa("João", "Silva"));
+hospedes.Add(new Pessoa("Maria", "Oliveira"));
+hospedes.Add(new Pessoa("Ana", "Souza"));
+
+Suite suite = new("Luxo", 3, 200.00m);
+
+Reserva reserva = new(5);
+
+reserva.CadastrarSuite(suite);
+reserva.CadastrarHospedes(hospedes);
+
+Console.WriteLine($"Quantidade de hóspedes: {reserva.ObterQuantidadeHospedes()}");
+Console.WriteLine($"Valor da diária: {reserva.CalcularValorDiaria():C}");
+
